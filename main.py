44ba2872895b7774e251886a8af1e50ac708cf49
Samuel Ogogo <65853838+samosanction@users.noreply.github.com>
2023-06-18 20:38:02 +0100

    Scrape Redit Post code

---
+++ main.py
@@ -0,0 +1,14 @@
+import praw
+
+reddit = praw.Reddit(user_agent=True, client_id="YOUR REDDIT APP ID",
+  client_secret="YOUR REDDIT APP SECRET", username='YOUR REDDIT USERNAME', password='YOUR REDDIT ACCOUNT PASSWORD')
+
+url = "https://www.reddit.com/r/cats/comments/qz201u/declawing_hurts_your_cat/"
+
+post = reddit.submission(url=url)
+print(post.title)
+print(post.selftext)
+
+print(len(post.comments))
+for comment in post.comments:
+  print(comment.body)
