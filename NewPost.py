c9126a60c123179499ee739ba59cb50a5974e4ea
Samuel Ogogo <65853838+samosanction@users.noreply.github.com>
2023-06-19 16:21:56 +0100

    Create a post for a subredit

---
+++ NewPost.py
@@ -0,0 +1,17 @@
+
+import praw
+
+
+reddit = praw.Reddit(user_agent=True, client_id="YOUR REDDIT APP ID",
+  client_secret="YOUR REDDIT APP SECRET", username='YOUR REDDIT USERNAME', password='YOUR REDDIT ACCOUNT PASSWORD')
+
+subreddit = reddit.subreddit("pythonsandlot")
+subreddit.validate_on_submit = True
+
+title = 'This is my new Python Bot submission'
+content = """
+Hey, I am just trying out Python!
+This is my first post!
+"""
+
+subreddit.submit(title=title, selftext=content)
